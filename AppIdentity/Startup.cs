92d849f6558c29ae536107fd82a8fe615ce98615
Thomas Christensen <Thomas.Christensen@dpi.wi.gov>
2018-10-28 19:59:15 -0500

    adding quick start views

--- AppIdentity/Startup.cs
+++ AppIdentity/Startup.cs
@@ -11,6 +11,8 @@ namespace AppIdentity
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
+
             services.AddIdentityServer()
                 .AddDeveloperSigningCredential()
                 .AddInMemoryApiResources(Config.GetApiResources())
@@ -36,6 +38,8 @@ namespace AppIdentity
 
             app.UseIdentityServer();
 
+            app.UseStaticFiles();
+            app.UseMvcWithDefaultRoute();
             //app.Run(async (context) =>
             //{
             //    await context.Response.WriteAsync("Hello World!");
